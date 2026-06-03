ef1d2a5bc1e49f086a3731853e214eb553ac0b1f
Rroix <averageworkfan@gmail.com>
2026-06-03 23:47:08 +0200

    Por poco se me olvida cambiar las cosas a inglés

--- commands/utility/registrar.js
+++ commands/utility/registrar.js
@@ -2,11 +2,11 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("registrar")
-    .setDescription("Introduce las coordenadas para registrar'")
+    .setName("register")
+    .setDescription("Introduce new coordinates to register'")
     .addStringOption((option) => 
         option
-            .setName("coordenadas")
+            .setName("coordinates")
             .setDescription("X, Y, Z or X, Z")
             .setRequired(true)
             .setMaxLength(26), // Lo que he contado como las coordenadas más alejadas del World Border en caracteres
@@ -15,7 +15,7 @@ module.exports = {
     .addStringOption((option) => 
         option
             .setName("dimension")
-            .setDescription("La dimensión de las coordenadas")
+            .setDescription("The dimension of the coordinates")
             .setRequired(true)
             .setChoices(
                 { name: "Overworld", value: "overworld_dimension" },
@@ -25,7 +25,7 @@ module.exports = {
         ),
 
     async execute(interaction) {
-        const coordinates_from_user = interaction.options.getString("coordenadas");
+        const coordinates_from_user = interaction.options.getString("coordinates");
         const dimension = interaction.options.getString("dimension");
 
         const coordinates_untrimmed = coordinates_from_user.split(",")
