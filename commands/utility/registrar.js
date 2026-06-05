acef4629c93bb7df2750b2c917e9aff3276baa7e
B-M198 <vf.martirodriguez@gmail.com>
2026-06-05 16:17:20 +0200

    A ver

--- commands/utility/registrar.js
+++ commands/utility/registrar.js
@@ -1,3 +1,4 @@
+//registrar.js
 const { SlashCommandBuilder } = require("discord.js");
 let prisma;
 
@@ -34,16 +35,15 @@ module.exports = {
         const coordinates = interaction.options.getString("coordenadas");
         const dimension = interaction.options.getString("dimension");
 
-
         // Declaro las variables, me acabo de enterar de que las variables declaradas dentro de ifs no persisten,
         // pero los valores asignados dentro de ifs si.
 
         let x_coordinates;
         let y_coordinates;
-        let z_coordinates
+        let z_coordinates;
 
         // True if the coordinate has weird values we don't accept
-        const Has_not_numeric_characters = /[^0-9,]/.coordinatest(str);
+        const Has_not_numeric_characters = /[^0-9,]/.getString(coordinatest());
 
         // Para ver si no han introducido los datos necesarios
         if (!coordinates_untrimmed[0] || !coordinates_untrimmed[1])  {
@@ -67,10 +67,10 @@ module.exports = {
             z_coordinates = coordinates_untrimmed[2].trim()
         }
 
-
+ 
         // Aquí iría la conexión con el prisma.js y todas esas cosiñas ~ Se aprecia el galego ahí :3
         try{
-            await prisma.guildConfig.upsert({ //upsert -> si hay valor, sustituye; si no hay, crea
+            await prisma.cords.upsert({ //upsert -> si hay valor, sustituye; si no hay, crea
                 where: {
                     guildId: interaction.guildId //cargamos el servidor para poder expandir en un futuro
                 },
